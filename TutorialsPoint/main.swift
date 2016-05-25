45de63d1978d4a6908eb8919160b31851982b288
Hemita Pathak <hemita.pathak@gmail.com>
2016-05-25 11:04:25 +0530

    Initial Commit

---
+++ TutorialsPoint/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TutorialsPoint
+//
+//  Created by Hemita Pathak on 25/05/16.
+//  Copyright © 2016 InformationWorks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
